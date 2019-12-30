ff8f0eeed79856a7339d5ba8cb4b6ce488f6b709
Mohamed Said Mosli Bouksiaa <mohamed.mosli-bouksiaa@fabernovel.com>
2019-12-30 23:58:09 +0100

    Complete MovieApi
    Change-Id: I4bb4fab45fc1520d8b37273e22d70792a10c199b

--- app/src/main/java/com/fabernovel/codingdojo/data/MovieApi.java
+++ app/src/main/java/com/fabernovel/codingdojo/data/MovieApi.java
@@ -2,9 +2,31 @@ package com.fabernovel.codingdojo.data;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 interface MovieApi {
+
+    @GET("/3/genre/{id}")
+    Call<RestGenre> getGenre(
+        @Path("id") Integer id,
+        @Query("api_key") String apiKey,
+        @Query("language") String language
+    );
+
+    @GET("/3/discover/movie")
+    Call<RestDiscoverResponse> getMoviesInTheatre(
+        @Query("api_key") String apiKey,
+        @Query("primary_release_date.gte") String fromDate,
+        @Query("primary_release_date.lte") String toDate,
+        @Query("language") String language
+    );
+
+    @GET("/3/discover/movie?sort_by=vote_average.desc")
+    Call<RestDiscoverResponse> getTopMovies(
+        @Query("api_key") String apiKey,
+        @Query("primary_release_year") Integer year
+    );
 
     @GET("/3/discover/movie")
     Call<RestDiscoverResponse> getUpcomingMovies(
---
+++ app/src/main/java/com/fabernovel/codingdojo/data/RestGenre.java
@@ -0,0 +1,11 @@
+package com.fabernovel.codingdojo.data;
+
+class RestGenre {
+
+    private int id;
+    private String name;
+
+    String getGenreName() {
+        return name;
+    }
+}
