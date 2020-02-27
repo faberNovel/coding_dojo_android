c96d12fb6b18d3d08b790350ce7f73cdaaa846b4
Mohamed Said Mosli Bouksiaa <mohamed.mosli-bouksiaa@fabernovel.com>
2020-02-27 16:31:57 +0100

    Add MoviesAdapter
    Change-Id: I19c415bc4095d78d4608dfaffb42eb09a9b02e5e

---
+++ app/src/main/java/com/fabernovel/codingdojo/app/main/MoviesAdapter.java
@@ -0,0 +1,34 @@
+package com.fabernovel.codingdojo.app.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.fabernovel.codingdojo.R;
+import com.fabernovel.codingdojo.entity.Movie;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+    private final List<Movie> dataSet = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+            .from(parent.getContext())
+            .inflate(R.layout.item_movie, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        holder.bind(dataSet.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
